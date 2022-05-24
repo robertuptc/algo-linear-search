4a327cacec1dc3bb84888f79ac99afc7604d4999
robertuptc <robertuptc@gmail.com>
2022-05-24 19:05:40 -0400

    finished exercise

--- js/linearSearch.js
+++ js/linearSearch.js
@@ -4,6 +4,22 @@ for (let i = 1; i <= 1000; i++) {
 }
 
 exports.linearSearch = function(valueToFind, arrayToSearchThrough) {
-
+    for (let i = 0; i < arrayToSearchThrough.length; i++) {
+        if (arrayToSearchThrough[i] === valueToFind) {
+            return i
+        }
+    }
 };
+
+
+
+exports.linearSearchGlobal = function(valueToFind, arrayToSearchThrough) {
+    let answer = []
+    for (let i = 0; i < arrayToSearchThrough.length; i++) {
+        if (arrayToSearchThrough[i] === valueToFind) {
+            answer.push(i)
+        }
+    } return answer 
+};
+
 
