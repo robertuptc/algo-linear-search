0be1a107edc4e514925ccdf4f083095d60f405ed
robertuptc <robertuptc@gmail.com>
2022-05-25 19:08:34 -0400

    finished exercise

--- python/linear_search.py
+++ python/linear_search.py
@@ -3,4 +3,22 @@ for number in range(1, 1001):
     array_to_search_through.append(number)
 
 def linear_search(value_to_find, array_to_search_through):
-    # your code here
+    for x in range(0, len(array_to_search_through), 1):
+        if value_to_find == array_to_search_through[x]:
+            return x
+            
+    
+
+
+def linear_search_global(value_to_find, array_to_search_through):
+    answer = []
+    for x in range(0, len(array_to_search_through), 1):
+        if value_to_find == array_to_search_through[x]:
+            answer.append(x)
+    return answer
+    
+
+
+
+
+
